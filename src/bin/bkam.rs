7e5ab516931d75d4cd34957af963fbd095b47d70
rnleach <rnleach@users.noreply.github.com>
2018-10-09 14:35:35 -0600

    Working on bkam fix.

--- src/bin/bkam.rs
+++ src/bin/bkam.rs
@@ -249,6 +249,9 @@ fn run() -> Result<(), Error> {
                             " specified, then data for all times is purged. This can be combined",
                             " with 'model' and 'site' arguments.")),
                 )
+        ).subcommand(
+            SubCommand::with_name("fix")
+                .about("Find and fix inconsistencies in the archive.")
         );
 
     let matches = app.get_matches();
@@ -265,6 +268,7 @@ fn run() -> Result<(), Error> {
         ("export", Some(sub_args)) => export(root, sub_args)?,
         ("import", Some(sub_args)) => import(root, sub_args)?,
         ("purge", Some(sub_args)) => purge(root, sub_args)?,
+        ("fix", Some(sub_args)) => fix(root, sub_args)?,
         _ => unreachable!(),
     }
 
@@ -678,6 +682,7 @@ fn purge(root: &PathBuf, sub_args: &ArgMatches) -> Result<(), Error> {
             let all_runs = model.all_runs(&after, &before);
 
             for run in all_runs {
+                println!("  Removing {} {} {}.", site, model.as_static(), run);
                 match arch.remove_file(site, model, &run) {
                     Ok(()) => {}
                     Err(_) => {}
@@ -685,6 +690,61 @@ fn purge(root: &PathBuf, sub_args: &ArgMatches) -> Result<(), Error> {
             }
         }
     }
+
+    Ok(())
+}
+
+fn fix(root: &PathBuf, _sub_args: &ArgMatches) -> Result<(), Error> {
+    // Check that the root exists.
+    println!("Checking if the archive location exists.");
+    if !root.as_path().is_dir() {
+        println!("Archive root directory not found. Quitting.");
+        return Err(err_msg("Invalid root."));
+    } else {
+        println!("Found, moving on.\n");
+    }
+
+    // Check that the data directory exists
+    println!("Checking for the data directory within the archive.");
+    let data_dir = &root.join("data");
+    if !data_dir.as_path().is_dir() {
+        println!("Archive data directory not found. Archive is empty. Quitting.");
+        return Err(err_msg("Invalid data directory."));
+    } else {
+        println!("Found, moving on.\n");
+    }
+
+    // Check if there is a database, if not, create it!
+    println!("Trying to connect to the archive file index (database).");
+    let arch = match Archive::connect(root) {
+        Ok(arch) => {
+            println!("Found the archive file index. Moving on.\n");
+            arch
+        }
+        Err(err) => {
+            println!(
+                "Error connecting to archive database {}. Trying to create a new database.\n",
+                err
+            );
+            Archive::create_new(root)?
+        }
+    };
+
+    // Check that all the files listed in the index are also in the data directory
+    println!("Checking index for non-existent files.");
+    let messages = arch.clean_index()?;
+    for message in messages {
+        println!("     {}", message);
+    }
+    println!("Done.\n");
+
+    // Check for extra files
+    println!("Checking that files in data directory belong and are in the index.");
+    let messages = arch.clean_data()?;
+    for message in messages {
+        println!("     {}", message);
+    }
+    println!("Done");
 
     Ok(())
 }
